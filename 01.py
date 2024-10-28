c0c8b907b9d233106f6e71f729b751614311d660
AnaRitaRangel <106035404+AnaRitaRangel@users.noreply.github.com>
2024-10-27 22:05:58 -0300

    Class 04 Python Curso em Video: Add file 01.py

---
+++ 01.py
@@ -0,0 +1,5 @@
+## Develop a program that asks for the user name, age and weight and prints all this information
+name = input("Type your name: ")
+age = input("Type your age: ")
+weight = input("Type your weight in kilos: ")
+print(f"{name} is {age} years old and weights {weight} kilos")
