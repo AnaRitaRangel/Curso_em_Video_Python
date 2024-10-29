c1dedd19ee80c95e84e5d43e29f77797c967afbb
AnaRitaRangel <106035404+AnaRitaRangel@users.noreply.github.com>
2024-10-29 20:33:27 -0300

    Class 08 file 04.py

---
+++ 04.py
@@ -0,0 +1,12 @@
+## CLASS 08 ##
+print("==== Square Root ====")
+from math import sqrt #Import just the sqrt method from the math library
+n = float(input("Type a number: "))
+square_rt = sqrt(n)
+print(f"The square root of {n} is {square_rt:.2f}")
+
+
+import math #Import the whole library
+n2 = float(input("Type a number: "))
+square_root = math.sqrt(n2)
+print(f"The square root of {n2} is {square_root:.2f}")
